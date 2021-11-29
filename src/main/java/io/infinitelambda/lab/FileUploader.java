f11cc2ea84853eee6e724336c1e956550d2b61a6
Assia Hristova <assiaphristova@gmail.com>
2021-11-29 15:18:34 +0200

    created new class UploadToCloudProvider and Validator

--- src/main/java/io/infinitelambda/lab/FileUploader.java
+++ src/main/java/io/infinitelambda/lab/FileUploader.java
@@ -8,18 +8,9 @@ import static java.nio.file.Files.probeContentType;
 
 
 public class FileUploader {
-    public static UploadResult uploadToS3(java.io.File file, String contentType) {
-        System.out.println("Uploading to AWS S3");
-        // you don't need to implement to logic to upload
-        return new UploadResult();
-    }
 
-    public static boolean validateParameters(String[] args) {
-        if (args.length == 0 || args.length > 2) {
-            throw new IllegalArgumentException("Not enough parameters");
-        }
-        return true;
-    }
+
+
 
     public String getContentType(File file) {
         Path path = file.toPath();
--- src/main/java/io/infinitelambda/lab/Main.java
+++ src/main/java/io/infinitelambda/lab/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
         FileUploader fileUploader = new FileUploader();
 
-        if (FileUploader.validateParameters(args)){
+        if (Validator.validateParameters(args)){
             File file1 = new File(args[1]);
             String cloudProvider = args[0];
             String contentType1 = fileUploader.getContentType(file1);
             if (cloudProvider.equals("AWS S3")){
-                UploadResult result = FileUploader.uploadToS3(file1, contentType1);
+                UploadResult result = UploadToCloudProvider.uploadToS3(file1, contentType1);
+            }
+            else if (cloudProvider.equals("Azure")){
+                UploadResult result = UploadToCloudProvider.uploadToAzure(file1, contentType1);
             }
 
             System.out.println("Upload finished");
