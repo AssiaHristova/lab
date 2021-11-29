870f23a2f146a5a8d10fc2f7ec832425ffbc3e59
Assia Hristova <assiaphristova@gmail.com>
2021-11-29 13:59:21 +0200

    Initial commit

---
+++ src/main/java/io/infinitelambda/lab/Main.java
@@ -0,0 +1,23 @@
+package io.infinitelambda.lab;
+import java.io.File;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        FileUploader fileUploader = new FileUploader();
+
+        if (FileUploader.validateParameters(args)){
+            File file1 = new File(args[1]);
+            String cloudProvider = args[0];
+            String contentType1 = fileUploader.getContentType(file1);
+            if (cloudProvider.equals("S3")){
+                UploadResult result = FileUploader.uploadToS3(file1, contentType1);
+            }
+
+            System.out.println("Upload finished");
+        }
+
+
+
+    }
+}
---
+++ src/main/java/io/infinitelambda/lab/UploadResult.java
@@ -0,0 +1,10 @@
+package io.infinitelambda.lab;
+
+
+
+public class UploadResult {
+
+    // fields such as key, url, id...
+
+    // should we implement those fields - key, url, id?
+}
